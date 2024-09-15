1a0d93dec642528c3186d2381c50ec569b6699f4
HalcyonJX <502229027@qq.com>
2024-09-15 23:30:24 +0800

    Initial commit

---
+++ user-center-backend/src/main/java/com/guyue/usercenterbackend/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.guyue.usercenterbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.guyue.usercenterbackend.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+
+}
